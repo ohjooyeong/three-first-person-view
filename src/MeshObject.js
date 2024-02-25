ff1f4b3758c98d4cc575839bbb5ccb975b28cecd
OH-JOO-YEONG <brb1111@naver.com>
2024-02-25 19:30:38 +0900

    [Feat] MeshObject class 구현 및 3D모델 로드 적용

---
+++ src/MeshObject.js
@@ -0,0 +1,49 @@
+import { BoxGeometry, Mesh, MeshLambertMaterial } from "three";
+
+export class MeshObject {
+  constructor(info) {
+    this.name = info.name;
+    this.width = info.width || 1;
+    this.height = info.height || 1;
+    this.depth = info.depth || 1;
+    this.color = info.color || "white";
+    this.diffrenceY = info.diffrenceY || 0.4;
+    this.x = info.x || 0;
+    this.y = info.y || this.height / 2 + this.diffrenceY;
+    this.z = info.z || 0;
+
+    if (info.modelSrc) {
+      // GLB
+      info.loader.load(
+        info.modelSrc,
+        (glb) => {
+          glb.scene.traverse((child) => {
+            if (child.isMesh) {
+              child.castShadow = true;
+            }
+          });
+          info.scene.add(glb.scene);
+          glb.scene.position.set(this.x, this.y, this.z);
+        },
+        (xhr) => {
+          console.log("loading...");
+        },
+        (error) => {
+          console.log("error");
+        }
+      );
+    } else {
+      const geometry = new BoxGeometry(this.width, this.height, this.depth);
+      const material = new MeshLambertMaterial({
+        color: this.color,
+      });
+
+      this.mesh = new Mesh(geometry, material);
+      this.mesh.castShadow = true;
+      this.mesh.receiveShadow = true;
+      this.mesh.position.set(this.x, this.y, this.z);
+
+      info.scene.add(this.mesh);
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { MeshObject } from "./MeshObject";
 
 // Renderer
 const canvas = document.querySelector("#three-canvas");
@@ -10,6 +12,7 @@ const renderer = new THREE.WebGL1Renderer({
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
 renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 // Scene
 const scene = new THREE.Scene();
@@ -23,57 +26,102 @@ const camera = new THREE.PerspectiveCamera(
   1000 // far
 );
 
-camera.position.set(-3, 3, 7);
+camera.position.set(0, 3, 7);
 scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
+const loader = new GLTFLoader();
 
 // Light
 const ambientLight = new THREE.AmbientLight("white", 1);
-scene.add(ambientLight);
-
-const directionalLight = new THREE.DirectionalLight("white", 3);
-directionalLight.position.set(-3, 5, 1);
-directionalLight.castShadow = true;
-scene.add(directionalLight);
+const pointLight = new THREE.PointLight("white", 100, 100);
+pointLight.castShadow = true;
+pointLight.shadow.mapSize.width = 2048;
+pointLight.shadow.mapSize.height = 2048;
+pointLight.position.y = 10;
+scene.add(ambientLight, pointLight);
 
 // Mesh
-const boxMesh = new THREE.Mesh(
-  new THREE.BoxGeometry(2, 2, 2),
-  // new THREE.MeshBasicMaterial({ color: "firebrick" })
-  new THREE.MeshLambertMaterial({ color: "firebrick", side: THREE.DoubleSide })
-);
-boxMesh.position.y = 1;
-boxMesh.castShadow = true;
 
-const groundMesh = new THREE.Mesh(
-  new THREE.PlaneGeometry(10, 10),
-  // new THREE.MeshBasicMaterial({ color: "#092e66" })
-  new THREE.MeshLambertMaterial({ color: "#092e66", side: THREE.DoubleSide })
-);
-groundMesh.rotation.x = THREE.MathUtils.degToRad(-90);
-groundMesh.receiveShadow = true;
+const ground = new MeshObject({
+  scene,
+  name: "ground",
+  width: 50,
+  height: 0.1,
+  depth: 50,
+  color: "#092e66",
+  y: -0.05,
+  diffrenceY: "0",
+});
 
-scene.add(boxMesh, groundMesh);
+const floor = new MeshObject({
+  scene,
+  name: "floor",
+  width: 5,
+  height: 0.4,
+  depth: 5,
+  diffrenceY: "0",
+});
 
-camera.lookAt(boxMesh.position);
+const wall1 = new MeshObject({
+  scene,
+  name: "wall1",
+  width: 5,
+  height: 3,
+  depth: 0.2,
+  z: -2.4,
+});
+
+const wall2 = new MeshObject({
+  scene,
+  name: "wall2",
+  width: 0.2,
+  height: 3,
+  depth: 4.8,
+  x: 2.4,
+  z: 0.1,
+});
+
+const desk = new MeshObject({
+  scene,
+  loader,
+  name: "desk",
+  width: 1.8,
+  height: 0.8,
+  depth: 0.75,
+  x: 1.2,
+  z: -1.9,
+  modelSrc: "/models/desk.glb",
+});
+
+const lamp = new MeshObject({
+  scene,
+  loader,
+  name: "lamp",
+  width: 0.5,
+  height: 1.8,
+  depth: 0.5,
+  z: -1.7,
+  modelSrc: "/models/lamp.glb",
+});
+
+const roboticVaccum = new MeshObject({
+  scene,
+  loader,
+  name: "roboticVaccum",
+  width: 0.5,
+  height: 0.1,
+  depth: 0.5,
+  x: -1,
+
+  modelSrc: "/models/vaccum.glb",
+});
 
 // Draw
 const clock = new THREE.Clock();
 function draw() {
-  // boxMesh.position.y += 0.01;
-  const delta = clock.getDelta();
-
-  boxMesh.position.y += delta;
-  if (boxMesh.position.y > 5) {
-    boxMesh.position.y = 1;
-  }
-
   renderer.render(scene, camera);
-  controls.update();
-
-  // window.requestAnimationFrame(draw);
   renderer.setAnimationLoop(draw);
 }
 
